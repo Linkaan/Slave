4c0bdf6769e2f7e5c2a1cf28a36585617ce0a5f3
Linus Styrén <linus122xbb@gmail.com>
2016-12-23 13:11:05 +0100

    added slave.c

---
+++ slave.c
@@ -0,0 +1,34 @@
+/*
+ *  fagelmatare-slave
+ *    Measures sensor values from sensehat and reports back to master
+ *  slave.c
+ *    Initialize communication with master and listen for events 
+ *****************************************************************************
+ *  This file is part of Fågelmataren, an embedded project created to learn
+ *  Linux and C. See <https://github.com/Linkaan/Fagelmatare>
+ *  Copyright (C) 2015-2016 Linus Styrén
+ *
+ *  Fågelmataren is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the Licence, or
+ *  (at your option) any later version.
+ *
+ *  Fågelmataren is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public Licence for more details.
+ *
+ *  You should have received a copy of the GNU General Public Licence
+ *  along with Fågelmataren.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+
+int
+main (void)
+{
+	ssize_t s;
+}
