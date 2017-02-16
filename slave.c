68147006e961abbea31d3e58f2f5ffb434c40791
Linus Styrén <linus122xbb@gmail.com>
2017-02-16 22:58:30 +0100

    added initial code

--- slave.c
+++ slave.c
@@ -22,13 +22,84 @@
  *  along with Fågelmataren.  If not, see <http://www.gnu.org/licenses/>.
  *****************************************************************************
  */
+#include <signal.h>
+#include <semaphore.h>
+#include <stdatomic.h>
+#include <pthread.h>
+#include <sys/timerfd.h>
+#include <sys/eventfd.h>
+#include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
+#include <errno.h>
+
+/* Non-zero means we should exit the program as soon as possible */
+static sem_t keep_going;
+
+/* Signal handler for SIGINT, SIGHUP and SIGTERM */
+static void
+handle_sig (int signum)
+{
+    struct sigaction new_action;
+
+    sem_post (&keep_going);
+
+    new_action.sa_handler = handle_sig;
+    sigemptyset (&new_action.sa_mask);
+    new_action.sa_flags = SA_RESTART;
+
+    sigaction (signum, &new_action, NULL);
+}
+
+/* Setup termination signals to exit gracefully */
+static void
+handle_signals ()
+{
+    struct sigaction new_action, old_action;
+
+    /* Turn off buffering on stdout to directly write to log file */
+    setvbuf (stdout, NULL, _IONBF, 0);
+
+    /* Set up the structure to specify the new action. */
+    new_action.sa_handler = handle_sig;
+    sigemptyset (&new_action.sa_mask);
+    new_action.sa_flags = SA_RESTART;
+    
+    /* Handle termination signals but avoid handling signals previously set
+       to be ignored */
+    sigaction (SIGINT, NULL, &old_action);
+    if (old_action.sa_handler != SIG_IGN)
+        sigaction (SIGINT, &new_action, NULL);
+    sigaction (SIGHUP, NULL, &old_action);
+    if (old_action.sa_handler != SIG_IGN)
+        sigaction (SIGHUP, &new_action, NULL);
+    sigaction (SIGTERM, NULL, &old_action);
+    if (old_action.sa_handler != SIG_IGN)
+        sigaction (SIGTERM, &new_action, NULL);
+}
 
 int
 main (void)
 {
 	ssize_t s;
+
+	/* Initialize keep_going as binary semaphore initially 0 */
+    sem_init (&keep_going, 0, 0);
+
+    handle_signals ();
+
+    sem_wait (&keep_going);
+
+    /* **************************************************************** */
+    /*                                                                  */
+    /*                      Begin shutdown sequence                     */
+    /*                                                                  */
+    /* **************************************************************** */
+    sem_destroy (&keep_going);
+
+    return 0;
 }
